4fd2195864a5a04570ff1e880eee8473628fda9a
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2025-05-20 01:38:36 +0000

    Atualização do código para a versão Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 //Jogo Batalha Naval
-//Versão: Novato
+//Versão: Aventureiro
 //Aluno: Washington Melo
 
 int main() {
     printf("*** Jogo Batalha Naval ***\n");
-    printf("*** Versão Novato - Tabuleiro 10x10 ***\n\n");
+    printf("*** Versão Aventureiro - Tabuleiro 10x10 ***\n\n");
 
     // Criando o tabuleiro 10x10
     int tabuleiro[10][10];
@@ -19,14 +19,24 @@ int main() {
     }
 
     // Posicionando o navio horizontal (3 casas)
-    tabuleiro[1][1] = 3; // Linha 1, Coluna B
-    tabuleiro[1][2] = 3; // Linha 1, Coluna C
     tabuleiro[1][3] = 3; // Linha 1, Coluna D
+    tabuleiro[1][4] = 3; // Linha 1, Coluna E
+    tabuleiro[1][5] = 3; // Linha 1, Coluna F
 
     // Posicionando o navio vertical (3 casas)
-    tabuleiro[4][5] = 3; // Linha 4, Coluna F
-    tabuleiro[5][5] = 3; // Linha 5, Coluna F
-    tabuleiro[6][5] = 3; // Linha 6, Coluna F
+    tabuleiro[4][8] = 3; // Linha 4, Coluna I
+    tabuleiro[5][8] = 3; // Linha 5, Coluna I
+    tabuleiro[6][8] = 3; // Linha 6, Coluna I
+
+// Posicionando o navio na diagonal principal (3 casas)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][i] = 3; // [0][0] ou A0, [1][1] ou B1, [2][2] ou C2
+    }
+
+    // Posicionando o navio na diagonal secundária (3 casas)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][9 - i] = 3; // [0][9] ou J0, [1][8] ou I1, [2][7] ou H2
+    }
 
     // Imprimindo o tabuleiro
     printf("  A B C D E F G H I J\n"); // Cabeçalho das colunas
