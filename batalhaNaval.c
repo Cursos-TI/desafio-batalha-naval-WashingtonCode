f47cb912cdcf3a32dd81852b788efed4714e46b4
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2025-05-20 01:35:58 +0000

    Atualização do índex das linhas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -18,20 +18,20 @@ int main() {
         }
     }
 
-    // Posicionando o navio horizontal (3 casas) na linha 2, colunas B, C, D
-    tabuleiro[1][1] = 3; // Linha 2, Coluna B
-    tabuleiro[1][2] = 3; // Linha 2, Coluna C
-    tabuleiro[1][3] = 3; // Linha 2, Coluna D
+    // Posicionando o navio horizontal (3 casas)
+    tabuleiro[1][1] = 3; // Linha 1, Coluna B
+    tabuleiro[1][2] = 3; // Linha 1, Coluna C
+    tabuleiro[1][3] = 3; // Linha 1, Coluna D
 
-    // Posicionando o navio vertical (3 casas) na coluna F, linhas 5, 6, 7
-    tabuleiro[4][5] = 3; // Linha 5, Coluna F
-    tabuleiro[5][5] = 3; // Linha 6, Coluna F
-    tabuleiro[6][5] = 3; // Linha 7, Coluna F
+    // Posicionando o navio vertical (3 casas)
+    tabuleiro[4][5] = 3; // Linha 4, Coluna F
+    tabuleiro[5][5] = 3; // Linha 5, Coluna F
+    tabuleiro[6][5] = 3; // Linha 6, Coluna F
 
     // Imprimindo o tabuleiro
     printf("  A B C D E F G H I J\n"); // Cabeçalho das colunas
     for (int i = 0; i < 10; i++) {
-        printf("%d ", i + 1); // Cabeçalho das linhas
+        printf("%d ", i); // Cabeçalho das linhas
         for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
